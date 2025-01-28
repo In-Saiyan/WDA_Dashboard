d7e421a9df41f52b0744e22d2ea574594f8d7fa1
Shivansh <68265106+Thunder-Blaze@users.noreply.github.com>
2025-01-28 19:58:52 +0530

    Sm (#1)
    * dashboard html css js (#5)
    
    * sidebar (#4)
    
    ---------
    
    Co-authored-by: jamvi08 <janhavi.1jain08@gmail.com>
    Co-authored-by: Insha-7 <insha0503@gmail.com>

--- js/cards.js
+++ js/cards.js
@@ -100,6 +100,12 @@
                 collapseAll();
             }
         });
+
+        function setActive(element) {
+            const items = document.querySelectorAll('.menu li');
+            items.forEach(item => item.classList.remove('active'));
+            element.classList.add('active');
+          }
     
 
 
